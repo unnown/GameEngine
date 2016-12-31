8f1948432c341aadc5a664bdf90e64b3410d5196
BuildTools <unconfigured@null.spigotmc.org>
2016-12-31 15:32:16 +0100

    Added node-json (json based db) Improved file handling

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const path = require('path');
+var JsonDB = require('node-json-db');
+
+var settingDB = new JsonDB("settings", true, true);
+var userDB = new JsonDB("users", true, true);
 
 var userLib = require('./assets/libs/user.js');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/assets/html/index.html');
+app.get('/*', function(req, res){
+  var url = req.url.split("?")[0];
+  url = url.replace(/\.\.\//g, "");
+  if (url === "/") url = "index.html";
+  var file = path.join(__dirname, "assets/html/", url);
+	
+  res.sendFile(file);
 });
 
 var mime = {
@@ -16,35 +26,6 @@ var mime = {
   ".jpg": "image/jpg",
   ".gif": "image/gif"
 };
-
-app.get('/', (function(req, res) {
-  var url = req.url.split("?")[0];
-  url = url.replace(/\.\.\//g, "");
-  if (url === "/") url = "html/index.html";
-  var file = path.join(__dirname, "assets/html/", url);
-  var ext = path.extname(url);
-  var ctype = mime[ext];
-  ctype = ctype || "text/plain";
-
-  var headersSent = false;
-  var stream = fs.createReadStream(file);
-  stream.on("error", function(error) {
-
-    res.writeHead(500, { "Content-Type": "text/plain" });
-    res.end(error.toString());
-
-  }).on("data", function(data) {
-
-    if (!headersSent) {
-      headersSent = true;
-      res.writeHead(200, { "Content-Type": ctype });
-    }
-
-    res.write(data);
-  }).on("end", function() {
-    res.end();
-  });
-}));
 
 io.on('connection', function(socket){
 
